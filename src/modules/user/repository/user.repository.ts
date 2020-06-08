f3aa3cc28350beba22938b9fd8ad225cedcd6642
Stiven Rodriguez <merq.rodriguez@udla.edu.co>
2020-06-08 16:45:39 -0500

    fat: add try catch to functions of user repository

--- src/modules/user/repository/user.repository.ts
+++ src/modules/user/repository/user.repository.ts
@@ -1,40 +1,59 @@
-import database from "../../../helpers/database.ts";
+import database from "../../../config/database.ts";
 import User from "../dto/user.dto.ts";
 
 class UserRepository {
-
-    async findAll(): Promise<any> {
-        return await database.query("SELECT id,name FROM users ORDER BY id ASC;")
+  
+  async findAll(): Promise<any> {
+    try {
+      return await database.query("SELECT id,name FROM users ORDER BY id ASC;");
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async findById(id: number): Promise<any> {
-        return await database.query({
-            text: "SELECT id,name FROM users WHERE id = $1;",
-            args: [id]
-        })
+  async findById(id: number): Promise<any> {
+    try {
+      return await database.query({
+        text: "SELECT id,name FROM users WHERE id = $1;",
+        args: [id],
+      });
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async create(user: User): Promise<any> {
-        return await database.query({
-            text: "INSERT INTO users (name) VALUES ($1)",
-            args: [user.name]
-        })
+  async create(user: User): Promise<any> {
+    try {
+      return await database.query({
+        text: "INSERT INTO users (name) VALUES ($1)",
+        args: [user.name],
+      });
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async update(user: User): Promise<any> {
-        return await database.query({
-            text: "UPDATE users set name = $1 where id = $2",
-            args: [user.name, user.id]
-        })
+  async update(user: User): Promise<any> {
+    try {
+      return await database.query({
+        text: "UPDATE users set name = $1 where id = $2",
+        args: [user.name, user.id],
+      });
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async delete(id: number): Promise<any> {
-        return await database.query({
-            text: "DELETE FROM users where id = $1",
-            args: [id]
-        })
+  async delete(id: number): Promise<any> {
+    try {
+      return await database.query({
+        text: "DELETE FROM users where id = $1",
+        args: [id],
+      });
+    } catch (error) {
+      throw error;
     }
-
+  }
 }
 
 export default new UserRepository();
